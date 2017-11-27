f9c92fe0a07c25bb2efe195dd41708123765742e
Mitchell Pleune <mitchpleune@gmail.com>
2017-11-27 12:57:09 -0500

    emacs: inital commit
    add irony config

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((c-mode
+  (flycheck-clang-include-path . ("." "./src"))
+  (flycheck-gcc-include-path . ("." "./src")))
+ (c++-mode
+  (flycheck-clang-include-path . ("." "./src"))
+  (flycheck-gcc-include-path . ("." "./src"))
+  (flycheck-clang-language-standard . "c++11")
+  (flycheck-gcc-language-standard . "c++11")
+ ))
